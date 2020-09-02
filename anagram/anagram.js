1e7326375894897893cf3a7d524d908a542d8d46
Kristina <kristina@PatsBook.local>
2020-09-02 17:23:50 -0600

    second commit

---
+++ anagram/anagram.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const findAnagrams = (str1, [str2]) => {
+  const normalize = str1 => {
+    
+  }
+}
